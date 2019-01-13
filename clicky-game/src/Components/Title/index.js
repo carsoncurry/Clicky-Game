849605aabdf94fedeaafee36e9caa1d9d7499eca
Carson Curry <carsoncurry52@gmail.com>
2019-01-13 16:51:03 -0600

    heroku commit

--- clicky-game/src/Components/Title/index.js
+++ clicky-game/src/Components/Title/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Title = props => (
     <div className="navbar bg-dark">
-        <a href="/" className="navbar-brand">
+        <a className="navbar-brand">
             <img src="http://franverona.com/loadgo/logos/spiderman.png" className="spider-signal" height="50px" alt="Spider Signal"></img>
             Welcome to the Spider-Verse!
         </a>
