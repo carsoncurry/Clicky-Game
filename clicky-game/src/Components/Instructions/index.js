7c60f8e6038cebd8ad2d3ba4685fa7caca408d7c
Carson Curry <carsoncurry52@gmail.com>
2019-01-13 17:08:50 -0600

    updated fonts

--- clicky-game/src/Components/Instructions/index.js
+++ clicky-game/src/Components/Instructions/index.js
@@ -3,10 +3,10 @@ import "./style.css"
 
 const Instructions = props => (
     <div className="container text-center col-md-12">
-        <h3>Someone has scattered the Spiders through the Multiverse!</h3>
-        <h6>Click on a spider to send them back to their home dimension!
+        <h2>Someone has scattered the Spiders through the Multiverse!</h2>
+        <h5>Click on a spider to send them back to their home dimension!
             Make sure you click all twelve spiders once (and only once) to win the game!
-        </h6>
+        </h5>
     </div>
 )
 
--- clicky-game/src/Components/Title/index.js
+++ clicky-game/src/Components/Title/index.js
@@ -3,10 +3,10 @@ import "./style.css";
 
 const Title = props => (
     <div className="navbar bg-dark">
-        <a className="navbar-brand">
+        <div className="navbar-brand text-light">
             <img src="http://franverona.com/loadgo/logos/spiderman.png" className="spider-signal" height="50px" alt="Spider Signal"></img>
             Welcome to the Spider-Verse!
-        </a>
+        </div>
         <ul className="navbar-nav scores">
             <li class="nav-item">
                 Score: {props.score}
